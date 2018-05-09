eb35b7387e2841fb03304e0817e765ab6435ad64
Eric <gzcisco720@gmail.com>
2018-05-10 09:52:10 +1000

    refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { AppComponent } from './app.component';
+import { AppComponent } from './AppComponents/app.component';
 import { AppRoutes } from './app.routing';
 import { CoreModule } from './Core/core.module';
-import { ValueComponent } from './value/value.component';
+import { ValueComponent } from './AppComponents/value/value.component';
 
 @NgModule({
   declarations: [
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,7 @@
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { MembersListComponent } from './Members/members-list/members-list.component';
 import { MemberDetailComponent } from './Members/member-detail/member-detail.component';
-import { ValueComponent } from './value/value.component';
+import { ValueComponent } from './AppComponents/value/value.component';
 import { AuthGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
